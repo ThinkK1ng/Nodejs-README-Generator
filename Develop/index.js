ff4610f149abd0803759574cc872f74ab761ffa1
ThinkK1ng <cla.givens@gmail.com>
2022-12-13 16:04:21 -0600

    added code and was able to answer questions in console using inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,81 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-// TODO: Create an array of questions for user input
-const questions = [];
+function init() {
+
+    const inquirer = require("inquirer");
+    const fs = require("fs");
+    // TODO: Create an array of questions for user input
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is a good Title for your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is the Description of your project?",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "How do you Install your application?",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "How do you Use your application?",
+            name: "usage",
+        },
+        {
+            type: "list",
+            message: "What License did you use for this repository?",
+            choices: ["MIT", "GNU General Public License 2.0", "Apache License 2.0", "GNU General Public License 3.0"],
+            name: "license",
+        },
+        {
+            type: "input",
+            message: "How can people Contribute to your project?",
+            name: "contributing",
+        },
+        {
+            type: "input",
+            message: "How do people update the tests for your project?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "What is your email address where users and contributors can send questions?",
+            name: "email"
+        },
+    ])
+        .then((response) => {
+            return fs.writeFile(`./${fileName.toLowercase().split(' ').join('')}.md`, data, (err) => {
+                if (err) {
+                    console.log(err)
+                }
+                console.log('Your README is complete... now go get some rest.');
+            })
+        });
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {
+//    
+// }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    let badge;
+  
+  switch (license) {
+    case "GNU AGPLv3":
+      badge = { name: "GNU+AGPLv3", color: "orange" };
+      break;
+    case "GNU GPLv3":
+      badge = { name: "GNU+GPLv3", color: "red" };
+      break;
+    case "GNU LGPLv3":
+      badge = { name: "GNU+LGPLv3", color: "blue" };
+      break;
+    case "Mozilla Public License 2.0":
+      badge = { name: "Mozilla+2.0", color: "yellow" };
+      break;
+    case "Apache License 2.0":
+      badge = { name: "Apache+2.0", color: "green" };
+      break;
+    case "MIT License":
+      badge = { name: "MIT", color: "brightgreen" };
+      break;
+    case "Boost Software License 1.0":
+      badge = { name: "Boost+Software+1.0", color: "yellowgreen" };
+      break;
+    case "The Unlicense":
+      badge = { name: "The+Unlicense", color: "blueviolet" };
+      break;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
